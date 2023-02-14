caee2b137fa27a09f8f378f5350b50d73d75ba59
Govard Volovets <g3ag@yandex.ru>
2023-02-14 03:03:09 +0300

    Changed Main.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
-    public double calculate(int sum, int term, double percent) {
+    public int calculate(int sum, int term, double percent) {
 
         double i = percent / 12 / 100;
         byte n = (byte) (term * 12);
         double k = (i * Math.pow((1 + i), n)) / (Math.pow((1 + i), n) - 1);
 
-        int payment = (int) Math.round(sum * k);
+        int payment = (int) (sum * k);
         return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 1 год / Процентная ставка: 9.99%");
+        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 1 год(а) / Процентная ставка: 9.99%");
         System.out.println("Ежемесячный платеж по кредиту: ");
-        System.out.println(service.calculate( 1_000_000, 1, 9.99) + " ₽");
+        System.out.println(service.calculate(1_000_000, 1, 9.99) + " ₽");
 
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 2 год / Процентная ставка: 9.99%");
+        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 2 год(а) / Процентная ставка: 9.99%");
         System.out.println("Ежемесячный платеж по кредиту: ");
-        System.out.println(service.calculate( 1_000_000, 2, 9.99) + " ₽");
+        System.out.println(service.calculate(1_000_000, 2, 9.99) + " ₽");
 
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 3 год / Процентная ставка: 9.99%");
+        System.out.println("Сумма кредита: 1 000 000 ₽ / Срок: 3 год(а) / Процентная ставка: 9.99%");
         System.out.println("Ежемесячный платеж по кредиту: ");
-        System.out.println(service.calculate( 1_000_000, 3, 9.99) + " ₽");
+        System.out.println(service.calculate(1_000_000, 3, 9.99) + " ₽");
 
     }
 }
